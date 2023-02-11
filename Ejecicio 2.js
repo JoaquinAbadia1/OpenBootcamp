01b358c3538cadaf3086371e4fd6345847e162a4
Joaquin <joaquinabadia04@gmail.com>
2023-02-11 18:36:27 -0300

    a

---
+++ Ejecicio 2.js
@@ -0,0 +1,11 @@
+const yo = ['Joaquin', 18, true];
+const miLibro = {
+    nombre: 'Maze Runner',
+    autor: 'James Dashner',
+    fecha: '21 de octubre de 2009',
+    URL: 'http://www.maze-runner.com',
+
+    
+}
+const date = new Date("agust 10, 2020");
+
